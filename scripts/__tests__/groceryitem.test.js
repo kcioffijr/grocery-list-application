e4f40bef134cb58c10ca17af2eb3620e47ac0849
Cioffi, Kevin <cioffijr.kevin@gmail.com>
2023-12-28 15:09:04 -0500

    Added unit-tests for the GroceryItem class.

--- scripts/__tests__/groceryitem.test.js
+++ scripts/__tests__/groceryitem.test.js
@@ -4,6 +4,14 @@ import { GroceryItem } from '../groceryitem.js';
 describe('GroceryItem', () => {
     it('should properly initialize properties', () => {
         const item = new GroceryItem('Apple');
-        expect(item.name).toBe('Apple'); // Use the getter property
+        expect(item.name).toBe('Apple');
+    });
+
+    it('should throw a TypeError when name is not a string', () => {
+        const createItemWithNonString = () => {
+            new GroceryItem(null);
+        };
+        expect(createItemWithNonString).toThrow(TypeError);
+        expect(createItemWithNonString).toThrow('Grocery item name should be of type string');
     });
 });
--- scripts/groceryitem.js
+++ scripts/groceryitem.js
@@ -1,12 +1,16 @@
 export class GroceryItem {
     constructor(name) {
         if (typeof name !== 'string') {
-            throw new TypeError('Grocery Item name must be a string');
+            throw new TypeError('Grocery item name should be of type string');
         }
         this._name = name; // Assign the name to the instance variable
     }
 
     get name() {
         return this._name;
+    }
+
+    toString() {
+        return this._name.toString();
     }
 };

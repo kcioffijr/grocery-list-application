11d12c9b656ea6e7d8f67e26ae93047a0108bf2b
Cioffi, Kevin <cioffijr.kevin@gmail.com>
2023-12-29 14:03:37 -0500

    Started with too much. Pivoting from TDD and OOP for this project.

--- scripts/grocerylist.js
+++
@@ -1,9 +0,0 @@
-export class GroceryList {
-    constructor(doc = document) {
-        this._list = doc.getElementsByClassName('item');
-    }
-
-    get list() {
-        return Array.from(this._list, element => element.textContent.trim());
-    }
-}
